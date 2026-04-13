e3e4c0750731f63fe51d1251ec2a0ed6a821b8c2
Razgrizsx <razgrisss41@gmail.com>
2026-04-13 08:50:16 -0300

    travel deleted

--- dist/src/travel/dto/travel-notice.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateTravelNoticeDto {
-    childId?: string;
-    destination: string;
-    departureDate: string;
-    returnDate: string;
-    contactPhone?: string;
-    contactEmail?: string;
-    notes?: string;
-}
